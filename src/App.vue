953098d81d14e6a951b6368efe958ea305087663
Andy <andy@Andys-MacBook-Pro.local>
2021-09-22 19:08:39 -0400

    wrong callback, but no error

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ function add(param: number) {
   console.log('add', param)
 }
 
+function foo(str: string) {
+  console.log(str)
+}
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <MyButton
-    @add-something="add"
+    @add-something="foo"
   ></MyButton>
 </template>
